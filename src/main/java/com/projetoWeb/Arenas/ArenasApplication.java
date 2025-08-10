f269d3d52d04bcec8bcb69b3b1b5707b1ac1b02f
GustavoTBett <gustavotaufembachbtt@gmail.com>
2025-08-10 01:22:12 -0300

    first commit

---
+++ src/main/java/com/projetoWeb/Arenas/ArenasApplication.java
@@ -0,0 +1,13 @@
+package com.projetoWeb.Arenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenasApplication.class, args);
+	}
+
+}
