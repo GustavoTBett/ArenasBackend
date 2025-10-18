0a0cc316f61e9789be7dac2698cb8e7eb24f5724
GustavoTBett <gustavotaufembachbtt@gmail.com>
2025-10-18 00:25:19 -0300

    subido correcao liberacao de urls

--- src/main/java/com/projetoWeb/Arenas/security/SecurityConfig.java
+++ src/main/java/com/projetoWeb/Arenas/security/SecurityConfig.java
@@ -79,9 +79,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/user/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/user/logout").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/user/refresh-token").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/logout").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth/refresh-token").permitAll()
                         .requestMatchers(HttpMethod.GET, "/user/me").permitAll()
                         .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated())
